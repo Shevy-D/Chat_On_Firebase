cf7a67ea6f2160a7ef52d5ae9469b291a12efae6
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-04-26 22:54:58 +0500

    Added authentication

--- app/src/main/java/com/shevy/chatonfirebase/MainActivity.kt
+++ app/src/main/java/com/shevy/chatonfirebase/MainActivity.kt
@@ -2,17 +2,43 @@ package com.shevy.chatonfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.shevy.chatonfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val database = Firebase.database
         val myRef = database.getReference("messages").child("Dmitriy")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
 
-        myRef.setValue("Hello, World!")
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Dmitriy: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/shevy/chatonfirebase/SingInAct.kt
@@ -0,0 +1,74 @@
+package com.shevy.chatonfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.shevy.chatonfirebase.databinding.ActivitySingInBinding
+
+class SingInAct : AppCompatActivity() {
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivitySingInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySingInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    account.idToken?.let { it -> firebaseAuthWithGoogle(it) }
+                }
+
+            } catch (e: ApiException) {
+                Log.d("MyLog", "Api exception")
+            }
+        }
+        binding.bSingIn.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun getClient(): GoogleSignInClient {
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signInWithGoogle() {
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("MyLog", "Google signIn done")
+            } else {
+                Log.d("MyLog", "Google signIn error")
+            }
+        }
+    }
+}
+
